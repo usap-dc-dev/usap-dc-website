b0cae7eaadc2a955b4befd5dcb72d33e1d1b41d7
Neville Shane <shanen314@gmail.com>
2018-04-24 15:42:51 +0000

    added instructions to uid2iso.py

--- uid2iso.py
+++ uid2iso.py
@@ -1,3 +1,7 @@
+# Creates an ISOXML file from a uid and puts it in watch directory
+# To run in production environment:
+# >sudo -E LD_LIBRARY_PATH=/opt/rh/python27/root/usr/lib64 /opt/rh/python27/root/usr/bin/python uid2iso.py <uid>
+
 import sys
 from lib.curatorFunctions import doISOXML
 
