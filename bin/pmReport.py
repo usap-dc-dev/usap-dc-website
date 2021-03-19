f974c89299425e97dba9ff01059327b0413f8f68
nevilleshane <shanen314@gmail.com>
2021-03-19 14:30:59 -0400

    new script to generate quarterly reports for project managers

---
+++ bin/pmReport.py
@@ -0,0 +1,343 @@
+#!/opt/rh/python27/root/usr/bin/python
+# Run as a cron task to generate quarterly reports for project managers
+# run from main usap-dc directory with python bin/pmReport
+
+import datetime
+from dateutil.relativedelta import relativedelta
+import time
+import psycopg2
+import psycopg2.extras
+import json
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+import os
+import shutil
+
+config = json.loads(open('/web/usap-dc/htdocs/config.json', 'r').read())
+config.update(json.loads(open('/web/usap-dc/htdocs/inc/report_config.json', 'r').read()))
+TMP_DIR = 'tmp'
+
+def connect_to_db():
+    #info = config['PROD_DATABASE'] - when running on dev server, so we can access prouction DB
+    info = config['DATABASE']
+    conn = psycopg2.connect(host=info['HOST'],
+                            port=info['PORT'],
+                            database=info['DATABASE'],
+                            user=info['USER'],
+                            password=info['PASSWORD'])
+    cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    return (conn, cur)
+
+
+def querySubmissionTable(cur, submission_type, status):
+    query = "SELECT * FROM submission WHERE submission_type = '%s' AND status = '%s';" % (submission_type, status)
+    cur.execute(query)
+    res = cur.fetchall()
+    msg = ""
+    for d in res:
+        url = config['CURATOR_PAGE'] % d['uid']
+        msg += """<li><a href="%s">%s</a> %s</li>""" % (url, d['uid'], d['submitted_date'].strftime('%Y-%m-%d'))
+    msg += """</ul>"""
+    return msg
+
+
+def sendEmail(message, subject, file=None):
+    print(subject)
+    sender = config['USAP-DC_GMAIL_ACCT']
+    recipients = config['RECIPIENTS']
+
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = ', '.join(recipients)
+
+    content = MIMEText(message, 'html', 'utf-8')
+    msg.attach(content)
+
+    if file: 
+        with open(file, "rb") as f:
+            part = MIMEApplication(
+                f.read(),
+                Name=os.path.basename(file)
+            )
+        # After the file is closed
+        part['Content-Disposition'] = 'attachment; filename="%s"' % os.path.basename(file)
+        msg.attach(part)
+
+    smtp_details = config['SMTP']
+    s = smtplib.SMTP(smtp_details["SERVER"], smtp_details['PORT'].encode('utf-8'))
+    # identify ourselves to smtp client
+    s.ehlo()
+    # secure our email with tls encryption
+    s.starttls()
+    # re-identify ourselves as an encrypted connection
+    s.ehlo()
+    s.login(smtp_details["USER"], smtp_details["PASSWORD"])
+    s.sendmail(sender, recipients, msg.as_string())
+    s.quit()  
+
+
+if __name__ == '__main__':
+    # get current date
+    today = datetime.date.today()
+
+    today = today - relativedelta(days=10)
+    three_months_ago = today - relativedelta(months=3)
+    (conn, cur) = connect_to_db()
+
+    # make tmp dir for csv files
+    os.mkdir(TMP_DIR)
+
+    query = "SELECT * FROM program"
+    cur.execute(query)
+    res = cur.fetchall()
+
+    for r in res:
+        program = r['id']
+        # if program != "Antarctic Organisms and Ecosystems": continue
+        send_report = False
+
+        title = "USAP-DC QUARTERLY REPORT FOR %s: %s TO %s" % (program.upper(), three_months_ago, today)
+
+        msg = """<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+                <title>%s</title>""" % title
+
+        msg = """<html><head>
+                        <style>
+                            table, th, td {
+                                border: 1px solid black;
+                                border-collapse: collapse;
+                            }
+                        </style>
+                </head><body><h1>%s</h1>""" % title
+
+        # new projects/data added and updated to DB
+        query = """SELECT project.proj_uid, project.title AS proj_title, project.date_created, JSON_AGG(a.*) awards,d.num_datasets, JSON(d.datasets::text) datasets
+                    FROM project
+                    JOIN project_award_map pam ON pam.proj_uid = project.proj_uid
+                    JOIN (
+                    	SELECT * FROM award
+                    	JOIN award_program_map apm ON apm.award_id = award.award
+                    	JOIN program ON program.id = apm.program_id
+                    	) a ON a.award = pam.award_id
+                    JOIN (
+                        SELECT proj_uid, JSON_AGG(pd.*) AS datasets , COUNT(pdm.dataset_id) AS num_datasets
+                        FROM project_dataset_map pdm 
+                        JOIN project_dataset pd ON pd.dataset_id = pdm.dataset_id
+                        GROUP BY proj_uid
+                    ) d ON d.proj_uid=project.proj_uid
+                    WHERE project.date_created >= '%s'
+                    AND a.program_id='%s'
+                    GROUP BY project.proj_uid,d.num_datasets,d.datasets::text
+                    ORDER BY project.date_created
+                    ;""" % (three_months_ago, program)
+        cur.execute(query)
+        res2 = cur.fetchall()
+        if len(res2) > 0: send_report = True
+
+        msg += """<h2>New Projects Added to USAP-DC in Last Quarter:</h2>"""
+        for p in res2:
+            url = config['PROJECT_LANDING_PAGE'] % p['proj_uid']
+            awards = ""
+            datasets = ""
+            for a in p['awards']:
+                awards += """<br>&emsp;<b>%s</b> (PEC: %s, PI: %s)""" % (a['award'], a['pec'], a['name'])
+            for d in p['datasets']:
+                datasets += """<br>&emsp;<a href='%s'>%s</a> (%s)""" % (d['url'], d['title'], d['repository'])
+            msg += """<b>Project Title:</b> %s<br>
+                      <b>Award(s):</b> %s<br>
+                      <b>Date Created:</b> %s<br>
+                      <b>Number of Datasets:</b> %s<br>
+                      <b>Dataset Links:</b> %s<br>
+                      <b>Project Landing Page:</b> %s <br><br>""" \
+                      % (p['proj_title'], awards, p['date_created'], p['num_datasets'], datasets, url)
+
+
+        # new datasets submitted to USAP-DC        
+        query = """SELECT dataset.title AS ds_title, dataset.date_created, award.*, dam.*, program.pec 
+                    FROM dataset
+                    JOIN dataset_award_map dam ON dam.dataset_id = dataset.id
+                    JOIN award ON award.award = dam.award_id
+                    JOIN award_program_map apm ON apm.award_id = award.award
+                    JOIN program ON program.id = apm.program_id
+                    WHERE dataset.date_created > '%s'
+                    AND apm.program_id='%s';""" % (three_months_ago, program)
+        cur.execute(query)
+        res2 = cur.fetchall()
+        if len(res2) > 0: send_report = True
+
+        msg += """<h2>New Datasets Added to USAP-DC in Last Quarter:</h2>"""
+        for d in res2:
+            url = config['DATASET_LANDING_PAGE'] % d['dataset_id']
+            msg += """<b>Dataset Title:</b> %s<br>
+                      <b>Award ID:</b> %s<br>
+                      <b>PI:</b> %s<br>
+                      <b>Award Title:</b> %s<br>
+                      <b>Date Created:</b> %s<br>
+                      <b>Dataset Landing Page:</b> %s <br><br>""" \
+                      % (d['ds_title'], d['award_id'], d['name'], unicode(d['title'], 'utf-8'), d['date_created'], url)
+
+
+        # new dataset links to project pages
+        query = """SELECT project.proj_uid,project.title AS proj_title, ppm.person_id AS proj_pi, project.date_created, project.date_modified, 
+                    JSON_AGG(a.*) awards, JSON(d.datasets::text) datasets, JSON(pubs.pubs::text) pubs
+                    FROM project
+                    JOIN project_person_map ppm ON ppm.proj_uid = project.proj_uid
+                    JOIN project_award_map pam ON pam.proj_uid = project.proj_uid
+                    JOIN (
+                    	SELECT * FROM award
+                    	JOIN award_program_map apm ON apm.award_id = award.award
+                    	JOIN program ON program.id = apm.program_id
+                    	) a ON a.award = pam.award_id
+                    JOIN (
+                        SELECT proj_uid, JSON_AGG(pd.*) AS datasets 
+                        FROM project_dataset_map pdm 
+                        JOIN project_dataset pd ON pd.dataset_id = pdm.dataset_id
+                        GROUP BY proj_uid
+                    ) d ON d.proj_uid=project.proj_uid
+                    JOIN (
+                        SELECT proj_uid, JSON_AGG(r.*) AS pubs 
+                        FROM project_ref_map prm 
+                        JOIN reference r ON r.ref_uid = prm.ref_uid
+                        GROUP BY proj_uid
+                    ) pubs ON pubs.proj_uid=project.proj_uid  
+                    WHERE project.date_modified >= '%s' AND date_modified != date_created
+                    AND ppm."role" IN ('Investigator and contact', 'Investigator')
+                    AND a.program_id='%s'
+                    GROUP BY project.proj_uid, d.datasets::text, pubs.pubs::text, ppm.person_id
+                    ORDER BY project.proj_uid;""" % (three_months_ago, program)
+        cur.execute(query)
+        res2 = cur.fetchall()
+        if len(res2) > 0: send_report = True
+
+        msg += """<h2>Projects Updated During Last Quarter:</h2>"""
+        for p in res2:
+            url = config['PROJECT_LANDING_PAGE'] % p['proj_uid']
+            awards = ""
+            datasets = ""
+            pubs = ""
+            for a in p['awards']:
+                awards += """<br>&emsp;<b>%s</b> (PEC: %s, PI: %s) - %s""" % (a['award'], a['pec'], a['name'], a['title'])
+            for d in p['datasets']:
+                datasets += """<br>&emsp;<a href='%s'>%s</a> (%s)""" % (d['url'], d['title'], d['repository'])
+            for r in p['pubs']:
+                pubs += """<br>&emsp;<i>- %s</i>""" % (r['ref_text'])
+
+            # use this output once we can show new datasets and new references
+            # msg += """<b>Project Title:</b> %s<br>
+            #           <b>Award(s):</b> %s<br>
+            #           <b>Dataset Links:</b> %s<br>
+            #           <b>Publications:</b> %s<br><br>
+            #           """ % (p['proj_title'], awards, datasets, pubs)        
+
+            msg += """<b>Project Title:</b> %s<br>
+                      <b>PI:</b> %s<br>
+                      <b>Project Landing Page:</b> %s<br><br>
+                      """ % (p['proj_title'], p['proj_pi'] , url)  
+
+
+        # datasets modified
+        # query = """SELECT dataset.title AS ds_title, dataset.date_created, dataset.date_modified, award.*, dam.*, program.pec 
+        #             FROM dataset
+        #             JOIN dataset_award_map dam ON dam.dataset_id = dataset.id
+        #             JOIN award ON award.award = dam.award_id
+        #             JOIN award_program_map apm ON apm.award_id = award.award
+        #             JOIN program ON program.id = apm.program_id
+        #             WHERE dataset.date_modified > '%s' AND date_modified != date_created
+        #             AND apm.program_id='%s';""" % (three_months_ago, program)
+        # cur.execute(query)
+        # res2 = cur.fetchall()
+        # if len(res2) > 0: send_report = True
+
+        # msg += """<h2>Datasets Updated in USAP-DC in Last Quarter:</h2>"""
+        # for d in res2:
+        #     url = config['DATASET_LANDING_PAGE'] % d['dataset_id']
+        #     msg += """<b>Dataset Title:</b> %s<br>
+        #               <b>Award ID:</b> %s<br>
+        #               <b>PEC:</b> %s<br>
+        #               <b>PI:</b> %s<br>
+        #               <b>Award Title:</b> %s<br>
+        #               <b>Date Updated:</b> %s<br>
+        #               <b>Dataset Landing Page:</b> %s <br><br>""" \
+        #               % (d['ds_title'], d['award_id'], d['pec'], d['name'], unicode(d['title'], 'utf-8'), d['date_modified'], url)      
+
+
+        # all active awards
+        query = """SELECT award.*, program.pec, p.proj_uid, d.num_datasets, nla.non_lead_awards FROM award
+                    JOIN award_program_map apm ON apm.award_id = award.award
+                    JOIN program ON program.id = apm.program_id
+                    LEFT JOIN project_award_map pam ON  pam.award_id = award.award
+                    LEFT JOIN project p ON p.proj_uid= pam.proj_uid
+                    LEFT JOIN (
+                                SELECT proj_uid, COUNT(dataset_id) AS num_datasets 
+                                FROM project_dataset_map pdm GROUP BY proj_uid
+                    ) d ON d.proj_uid=p.proj_uid
+                    LEFT JOIN (
+                  		  SELECT a2.award as lead_award, JSON_AGG(a1.award) as non_lead_awards 
+                  		  FROM award a1
+                  		  JOIN award a2 ON a2.award= a1.lead_award_id
+                  		  WHERE a1.is_lead_award = 'Non-Lead'
+                  		  GROUP BY a2.award
+                  	) nla ON nla.lead_award = award.award
+                    WHERE apm.program_id='%s'
+                    AND start <= '%s' AND expiry >= '%s'
+                    AND award.is_lead_award IN ('Standard', 'Lead')
+                    ORDER BY award;""" % (program, today, today)
+        cur.execute(query)
+        res2 = cur.fetchall()
+        if len(res2) > 0: send_report = True
+
+        # make csv file
+        filename = "%s_Active_Awards_%s_to_%s.tsv" % (program.replace(' ','_'), three_months_ago, today) 
+        filepath = os.path.join('tmp', filename)
+        tsv_file = open(filepath, 'w')
+        tsv_file.write("Award ID\tPEC\tPI\tAward Title\tAward Start\tAward Expiry\tNon-Lead Awards\tNumber of Datasets\tProject Landing Page\n")
+
+        msg += """<h2>Summary of All Active Awards:</h2>"""
+        msg += """<table><thead><tr>
+                    <th>Award ID</th>
+                    <th>PEC</th>
+                    <th>PI</th>
+                    <th>Award Title</th>
+                    <th>Award Start</th>
+                    <th>Award Expiry</th>
+                    <th>Non-Lead Awards</th>
+                    <th>Number of Datasets</th>
+                    <th>Project Landing Page</th>
+                </th></thead>"""
+        for a in res2:
+            if a['proj_uid']:
+                url = config['PROJECT_LANDING_PAGE'] % a['proj_uid']
+            else:
+                url = ''
+            if a['non_lead_awards']:
+                nla = ', '.join(a['non_lead_awards'])
+            else:
+                nla = ''
+            tsv_file.write("%s\t%s\t%s\t%s\t%s\t%s\t%s\t %s\t%s\n" % (a['award'], a['pec'], a['name'], unicode(a['title'], 'utf-8'), a['start'], a['expiry'], nla, a['num_datasets'], url))
+            msg += """<tr><td>%s</td>
+                        <td>%s</td>
+                        <td>%s</td>
+                        <td>%s</td>
+                        <td>%s</td>
+                        <td>%s</td>
+                        <td>%s</td>
+                        <td>%s</td>
+                        <td>%s</td>
+                    </tr>""" \
+                      % (a['award'], a['pec'], a['name'], unicode(a['title'], 'utf-8'), a['start'], a['expiry'], nla, a['num_datasets'], url)
+        msg += "</table>"
+        tsv_file.close()
+
+        msg += """</body></html>"""
+
+      
+
+        if send_report: sendEmail(msg, title, file=filepath)
+        # need to slow process down so email function doesn't get overwhelmed
+        time.sleep(1)
+
+    # remove tmp dir
+    shutil.rmtree(TMP_DIR)
