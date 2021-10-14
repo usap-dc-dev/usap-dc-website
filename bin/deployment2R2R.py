22f2bad4521afc7d1c5b9de11a8cea79d9e102f3
nevilleshane <shanen314@gmail.com>
2021-10-14 16:27:32 -0400

    update project_dataset and project_dataset_map tables

--- bin/deployment2R2R.py
+++ bin/deployment2R2R.py
@@ -12,6 +12,7 @@ import psycopg2
 import psycopg2.extras
 import json
 import requests
+import sys
 from gmail_functions import send_gmail_message
 
 
@@ -21,8 +22,9 @@ config.update(json.loads(open('/web/usap-dc/htdocs/inc/report_config.json', 'r')
 R2R_API = 'https://service.rvdata.us/api/cruise/cruise_id/'
 R2R_LANDING_PAGE = 'https://www.rvdata.us/search/cruise/'
 
+
 def connect_to_db():
-    # info = config['PROD_DATABASE'] # when running on dev server, so we can access prouction DB
+    # info = config['PROD_DATABASE'] # when running on dev server, so we can access production DB
     info = config['DATABASE']
     conn = psycopg2.connect(host=info['HOST'],
                             port=info['PORT'],
@@ -58,6 +60,7 @@ if __name__ == '__main__':
 
     for d in data:
         cruise = None
+        start = False
         # get cruise names from deployment_id
         dep_id = d['deployment_id'].replace('-','')
         ind = dep_id.upper().find('LMG')
@@ -83,8 +86,53 @@ if __name__ == '__main__':
                 # print('%s - %s:\nold:%s\nnew:%s\n\n') % (d['proj_uid'], d['deployment_id'], d['url'], landing_page)
                 msg += """<b>%s - %s</b>
                                <br><b>Old URL:</b> %s<br>
-                               <b>New URL:</b> %s<br><br>""" \
+                               <b>New URL:</b> %s<br>""" \
                                % (d['proj_uid'], d['deployment_id'], d['url'], landing_page)
+                start = True
+
+
+
+            # check if cruise is in project_dataset table, if not, add it
+            if d['url'] != '':
+                sql_cmd = """SELECT * FROM project_dataset where repository='R2R' AND url = %s;"""
+                cur.execute(sql_cmd, (d['url'],))
+                res = cur.fetchall()
+                if len(res) == 0:
+
+                    #first find the highest non-USAP-DC dataset_id already in the table
+                    query = "SELECT MAX(dataset_id) FROM project_dataset WHERE dataset_id::integer < 600000;"
+                    cur.execute(query)
+                    res = cur.fetchall()
+                    ds_id = int(res[0]['max']) + 1
+
+                    sql_cmd = """INSERT INTO project_dataset (dataset_id, repository, title, url, status) 
+                            VALUES ('%s', 'R2R', 'Expedition Data of %s', '%s', 'exists');""" % (ds_id, cruise, d['url'])
+                    cur.execute(sql_cmd)
+                    if not start:
+                        msg += """<b>%s - %s</b><br>""" % (d['proj_uid'], d['deployment_id'])
+                        start = True
+                    msg += "<b>Entry added to project_dataset table with dataset_id: %s</b><br>" % ds_id
+
+                    # add to project_dataset map
+                    sql_cmd = """INSERT INTO """
+                else:
+                    ds_id = res[0]['dataset_id']
+
+                # check if project dataset is in project_dataset_map
+                sql_cmd = """SELECT * FROM project_dataset_map where proj_uid = '%s' AND  dataset_id = '%s';""" % (d['proj_uid'], ds_id)
+                cur.execute(sql_cmd)
+                res = cur.fetchall()
+                if len(res) == 0:
+                    sql_cmd = """INSERT INTO project_dataset_map (proj_uid, dataset_id) VALUES (%s, %s);"""
+                    cur.execute(sql_cmd, (d['proj_uid'], ds_id))
+                    if not start:
+                        msg += """<b>%s - %s</b><br>""" % (d['proj_uid'], d['deployment_id'])
+                        start = True
+                    msg += "<b>Entry added to project_dataset_map</b><br>"
+
+            if start:
+                msg += "<br>"
+
     cur.execute('commit;')
     if msg != '':
         sendEmail(msg, "Updated Deployment R2R URLs")
