6f46c16e4bda6b2ce9f7f5ada36aaeecff1799fa
nevilleshane <shanen314@gmail.com>
2021-10-22 15:20:18 -0400

    write file_count_archive results in database

--- bin/file_count_archive.py
+++ bin/file_count_archive.py
@@ -5,8 +5,8 @@ file_count_archive.py
 
 @author: fnitsche & nshane
 This version to be run in /archive/usap-dc/dataset with
-> python file_count_archive <dir_name>
-Will return values to be entered manually in DB
+> python3 file_count_archive <dir_name>
+Will update dataset_file_info in database
 """
 
 import os
@@ -15,16 +15,31 @@ import tarfile
 import gzip
 import zipfile
 import mimetypes
+import psycopg2
+import psycopg2.extras
+import json
+import struct
+
+
+config = json.loads(open('/archive/usap-dc/dataset/scripts/config.json', 'r').read())
+
+
+def connect_to_db():
+    info = config['DATABASE']
+    conn = psycopg2.connect(host=info['HOST'],
+                            port=info['PORT'],
+                            database=info['DATABASE'],
+                            user=info['USER'],
+                            password=info['PASSWORD'])
+    cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    return (conn, cur)
 
 
 # get uncompressed file size of gzipped file
-def get_uncompressed_size(file):
-    fileobj = open(file, 'r')
-    fileobj.seek(-8, 2)
-    crc32 = gzip.read32(fileobj)
-    isize = gzip.read32(fileobj)  # may exceed 2GB
-    fileobj.close()
-    return isize
+def get_uncompressed_size(filename):
+    with open(filename, 'rb') as f:
+        f.seek(-4, 2)
+        return struct.unpack('I', f.read(4))[0]
 
 
 def usap_get_url_list_test():
@@ -90,11 +105,44 @@ def get_dir_info(topdir):
                 u_file_size += this_file_size    
 
     mime_types.discard(None) 
+    mime_pg = lst2pgarr(list(mime_types))
     # print("-> subdirs = ", subdir_count)
     print("-> file_count = ", file_count)
     print("-> file_size_on_disk = ", file_size)
     print("-> file_size_uncompressed: ", u_file_size)
-    print("-> mime_types: ", lst2pgarr(list(mime_types)))
+    print("-> mime_types: ", mime_pg)
+
+
+    # update database
+    ds_id = topdir.replace('_archived_data', '')
+    archive_dir = "archive/dataset/%s" % ds_id
+
+    (conn, cur) = connect_to_db()
+
+    # check if dataset_id already exist in table
+    sql_line = "Select * "\
+                "From dataset_file_info "\
+                "WHERE dataset_id = %s AND dir_name = %s;"
+    cur.execute(sql_line, (ds_id, archive_dir))
+    data = cur.fetchall()
+
+    # if no data returned use insert otherwise update
+    if not data:
+        sql_line = """INSERT INTO dataset_file_info 
+                   (dataset_id, dir_name, file_count, file_size_on_disk, file_size_uncompressed, mime_types) VALUES
+                   (%s, %s, %s, %s, %s, %s);"""
+        cur.execute(sql_line, (ds_id, archive_dir, file_count, file_size, u_file_size, mime_pg))
+
+    else:
+        sql_line = """UPDATE dataset_file_info SET 
+                   file_count = %s, file_size_on_disk = %s, file_size_uncompressed = %s, mime_types = %s
+                   WHERE dataset_id = %s AND dir_name = %s;"""
+                  
+        cur.execute(sql_line, (file_count, file_size, u_file_size, mime_pg, ds_id, archive_dir))
+    # Make the changes to the database persistent
+    conn.commit()
+    cur.close()
+    conn.close()
    
     # return file_count, file_size, u_file_size, list(mime_types)
 
