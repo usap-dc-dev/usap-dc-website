6d39cf3b59d9d1280142372d8e6a455758822b03
Alex Strong <astrong@ldeo.columbia.edu>
2023-03-17 14:14:48 +0000

    Streamlined query for updating keyword_usap table

--- bin/coalesce_usap_ieda_keywords.sql
+++ bin/coalesce_usap_ieda_keywords.sql
@@ -1,4 +1,1 @@
-INSERT INTO keyword_usap SELECT keyword_id, keyword_label, keyword_type_id, keyword_description FROM keyword_ieda WHERE keyword_label NOT IN (select keyword_label from keyword_usap);
-update keyword_usap set keyword_type_id = keyword_description where keyword_id like 'ik%';
-update keyword_usap set keyword_description = keyword_altlabel where keyword_id like 'ik%';
-update keyword_usap set keyword_altlabel = null where keyword_id like 'ik%';
+INSERT INTO keyword_usap (keyword_id, keyword_label, keyword_type_id, keyword_description) SELECT keyword_id, keyword_label, keyword_type_id, keyword_description FROM keyword_ieda WHERE keyword_label NOT IN (select keyword_label from keyword_usap);
