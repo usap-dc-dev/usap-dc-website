169f97c4ce244525be70169aea3af9c2f7e086c4
Alex Strong <astrong@ldeo.columbia.edu>
2023-03-22 16:55:25 +0000

    Automated downloading of GCMD keyword CSVs

--- bin/ingestGCMDInstruments.py
+++ bin/ingestGCMDInstruments.py
@@ -3,48 +3,46 @@
 # script to ingest gcmd_instruments from csv file into gcmd_instruments table
 # to run form main dir: >python bin/ingestGCMDInstruments
 
-import psycopg2
-import psycopg2.extras
-import json
+import util
 import csv
 
-config = json.loads(open('config.json', 'r').read())
 in_file = 'instruments.csv'
+csv_url = "https://gcmd.earthdata.nasa.gov/kms/concepts/concept_scheme/instruments/?format=csv&page_num=1&page_size=2000"
+
+conn, cur = util.connect_to_db()
 
 
-def connect_to_db():
-    info = config['DATABASE']
-    conn = psycopg2.connect(host=info['HOST'],
-                            port=info['PORT'],
-                            database=info['DATABASE'],
-                            user=info['USER_CURATOR'],
-                            password=info['PASSWORD_CURATOR'])
-    cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-    return (conn, cur)
-
-
-conn, cur = connect_to_db()
-
-
-# read in csv file
-rownum = 0
-with open(in_file, 'r', encoding='utf-8') as csvfile:
-    reader = csv.reader(csvfile, delimiter=",")
-    for row in reader:
-        if rownum > 1:
-        # generate ID
-            id = row[0].upper()
-            if row[1] and row[1] != '':
-                id += ' > '+row[1].upper()
-            if row[2] and row[2] != '':
-                id += ' > '+row[2].upper()
-            if row[3] and row[3] != '':
-                id += ' > '+row[3].upper()  
-            if row[4] and row[4] != '':
-                id += ' > '+row[4].upper()           
-            # add to database
-            sql = "INSERT INTO gcmd_instrument (id, category, class, type, subtype, short_name, long_name) VALUES (%s, %s, %s, %s, %s, %s, %s);"
-                    
-            cur.execute(sql, (id, row[0], row[1], row[2], row[3], row[4], row[5]))
-        rownum += 1
-cur.execute('COMMIT;')
+if util.download_to_file(csv_url, in_file):
+    # read in csv file
+    rownum = 0
+    rows_added = 0
+    with open(in_file, 'r', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile, delimiter=",")
+        for row in reader:
+            if rownum > 1:
+            # generate ID
+                id = row[0].upper()
+                if row[1] and row[1] != '':
+                    id += ' > '+row[1].upper()
+                if row[2] and row[2] != '':
+                    id += ' > '+row[2].upper()
+                if row[3] and row[3] != '':
+                    id += ' > '+row[3].upper()  
+                if row[4] and row[4] != '':
+                    id += ' > '+row[4].upper()
+                check_sql = "select count(id) as c from gcmd_instrument where id = %s"
+                cur.execute(check_sql, (id,))
+                count = int(cur.fetchone()['c'])
+                if count == 0:
+                    rows_added += 1
+                    # add to database
+                    sql = "INSERT INTO gcmd_instrument (id, category, class, type, subtype, short_name, long_name) VALUES (%s, %s, %s, %s, %s, %s, %s);"        
+                    cur.execute(sql, (id, row[0], row[1], row[2], row[3], row[4], row[5]))
+            rownum += 1
+    if rows_added == 1:
+        print("Added 1 entry to gcmd_instrument")
+    else:
+        print("Added %d entries to gcmd_instrument" % rows_added)
+    cur.execute('COMMIT;')
+else:
+    print("Error downloading %s from %s", in_file, csv_url)
--- bin/ingestGCMDPlatforms.py
+++ bin/ingestGCMDPlatforms.py
@@ -3,46 +3,45 @@
 # script to ingest gcmd_platforms from csv file into gcmd_platforms table
 # to run form main dir: >python bin/ingestGCMDPlatforms
 
-import psycopg2
-import psycopg2.extras
-import json
+import util
 import csv
 
-config = json.loads(open('config.json', 'r').read())
 platforms_file = 'platforms.csv'
+csv_url = "https://gcmd.earthdata.nasa.gov/kms/concepts/concept_scheme/platforms/?format=csv&page_num=1&page_size=2000"
 
 
-def connect_to_db():
-    info = config['DATABASE']
-    conn = psycopg2.connect(host=info['HOST'],
-                            port=info['PORT'],
-                            database=info['DATABASE'],
-                            user=info['USER_CURATOR'],
-                            password=info['PASSWORD_CURATOR'])
-    cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-    return (conn, cur)
+conn, cur = util.connect_to_db()
 
-
-conn, cur = connect_to_db()
-
-
-# read in csv file
-rownum = 0
-with open(platforms_file, 'r', encoding='utf-8') as csvfile:
-    reader = csv.reader(csvfile, delimiter=",")
-    for row in reader:
-        if rownum > 1:
-        # generate ID
-            platform_id = row[0].upper()
-            if row[1] and row[1] != '':
-                platform_id += ' > '+row[1].upper()
-            if row[2] and row[2] != '':
-                platform_id += ' > '+row[2].upper()
-            if row[3] and row[3] != '':
-                platform_id += ' > '+row[3].upper()            
-            # add to database
-            sql = "INSERT INTO gcmd_platform (id, basis, category, sub_category, short_name, long_name) VALUES (%s, %s, %s, %s, %s, %s);"
-                    
-            cur.execute(sql, (platform_id, row[0], row[1], row[2], row[3], row[4]))
-        rownum += 1
-cur.execute('COMMIT;')
+if util.download_to_file(csv_url, platforms_file):
+    # read in csv file
+    rownum = 0
+    rows_added = 0
+    with open(platforms_file, 'r', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile, delimiter=",")
+        for row in reader:
+            if rownum > 1:
+            # generate ID
+                platform_id = row[0].upper()
+                if row[1] and row[1] != '':
+                    platform_id += ' > '+row[1].upper()
+                if row[2] and row[2] != '':
+                    platform_id += ' > '+row[2].upper()
+                if row[3] and row[3] != '':
+                    platform_id += ' > '+row[3].upper()
+                check_sql = "select count(id) as c from gcmd_platform where id = %s"
+                cur.execute(check_sql, (platform_id,))
+                count = cur.fetchone()['c']
+                if count == 0:
+                    # add to database
+                    sql = "INSERT INTO gcmd_platform (id, basis, category, sub_category, short_name, long_name) VALUES (%s, %s, %s, %s, %s, %s);"
+                    cur.execute(sql, (platform_id, row[0], row[1], row[2], row[3], row[4]))
+                    #print(sql % (platform_id, row[0], row[1], row[2], row[3], row[4]))
+                    rows_added += 1
+            rownum += 1
+    cur.execute('COMMIT;')
+    if rows_added == 1:
+        print("Added 1 entry to gcmd_platform")
+    else:
+        print("Added %d entries to gcmd_platform" % rows_added)
+else:
+    print("Error downloading %s from %s" % (platforms_file, csv_url))
